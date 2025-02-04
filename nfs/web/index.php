6af1a14577e6b72a5ddbee46f4a226316d6d35dd
brivaro <brian.b.v.r.65@gmail.com>
2025-02-04 19:49:00 +0100

    html

---
+++ nfs/web/index.php
@@ -0,0 +1,14 @@
+<?php
+// index.php
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+</head>
+<body>
+    <h1>Bienvenido al cluster de Brian, se merece un pedazo de 10</h1>
+</body>
+</html>
